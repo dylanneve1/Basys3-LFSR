aec15346968322919b783f7d7bd53b343335a648
Dylan Neve <dylanneve1@gmail.com>
2025-03-11 16:30:45 +0000

    LFSR: adapt module for section C of the lab

--- LFSR.src/sources_1/new/lfsr.v
+++ LFSR.src/sources_1/new/lfsr.v
@@ -2,21 +2,26 @@
 
 module lfsr
     #(parameter seed = 10'h26E)
-    (input clk, sh_en, rst_n, output [9:0] Q_out);
+    (input clk, rst_n, output reg max_tick_reg, output [9:0] Q_out);
     
     reg [9:0] Q_state;
     wire [9:0] Q_ns;
+    wire Q_fb;
     
-    always @(posedge clk) begin
-        if (!rst_n)
-            Q_state <= seed;
-        else if (sh_en)
-            Q_state <= Q_ns;
-    end
-    
-    // Next State Logic
     assign Q_fb = Q_state[9] ^ Q_state[6];
-    assign Q_ns = {Q_state[8:0],Q_fb};
+    
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            Q_state <= seed;
+            max_tick_reg <= 1'b0;
+        end else begin
+            if ((Q_state != seed) && ({Q_state[8:0], Q_fb} == seed))
+                max_tick_reg <= 1'b1;
+            else
+                max_tick_reg <= 1'b0;
+            Q_state <= {Q_state[8:0], Q_fb};
+        end
+    end
     
     // Output Logic
     assign Q_out = Q_state;
